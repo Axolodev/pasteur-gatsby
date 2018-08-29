fe72fafe9c402902decfdd7abbf60f106d6f6877
Roberto Ruiz <rroberto.rruiz@gmail.com>
2018-08-29 14:14:49 -0500

    cambio en layout de noticias

--- src/pages/noticias.jsx
+++ src/pages/noticias.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
 import Layout from './../components/Layout';
+import { H1 } from '../components/Headers';
 
-function Noticias ({data}) {
+
+function getCleanNewsData(newsEdges) {
+  return newsEdges.map(({
+    node: {
+      frontmatter: {
+        title,
+        date,
+        path
+      }, 
+      excerpt
+    }
+  }) => ({
+    title,
+    date,
+    path,
+    excerpt
+  }));
+}
+
+function Noticias ({data: {news}}) {
+  const cleanNewsData = getCleanNewsData(news.edges);
+  console.table(cleanNewsData);
+
   return (
     <Layout>
-      Content
+      <H1>Noticias</H1>
+
     </Layout>
   )
 }
@@ -13,11 +37,16 @@ export default Noticias;
 
 export const query = graphql`
   query AllBlogEntries {
-    allMarkdownRemark(filter: {frontmatter: {type: {eq: "news"}}}) {
+    news: allMarkdownRemark(
+      sort: {fields: [frontmatter___date], order: ASC},
+      filter: {frontmatter: {type: {eq: "news"}}}
+    ) {
       edges {
         node {
           frontmatter {
             title
+            date
+            path
           }
           excerpt
         } 
